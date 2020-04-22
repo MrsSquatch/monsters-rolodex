cad56f2fb8325741180ed95b9bed869cb5216851
Amanda Pacheco <Amanda.Pacheco.contractor@hilton.com>
2020-04-21 17:29:43 -0700

    Switched to use hooks and async await.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState("");
+  const fetchMonsters = async () => {
+    const usersResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/users"
+    );
 
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }));
-  }
-
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value });
+    setMonsters(await usersResponse.json());
   };
 
-  render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter((monster) =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
+  const handleChange = (e) => {
+    setSearchField(e.target.value);
+  };
 
-    return (
-      <div className="App">
-        <h1>Monster Rolodex</h1>
-        <SearchBox
-          placeholder="search monsters"
-          handleChange={this.handleChange}
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    fetchMonsters(monsters);
+  });
+
+  const filteredMonsters = monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  );
+
+  return (
+    <div className="App">
+      <h1>Monster Rolodex</h1>
+      <SearchBox placeholder="search monsters" handleChange={handleChange} />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
